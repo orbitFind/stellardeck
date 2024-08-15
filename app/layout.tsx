19a46451ad0ea8216872af729f51c1edc026ef53
Waseef Mohammad Khan <waseef2010@gmail.com>
2024-08-15 22:44:44 +0100

    added manifest link

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>
         <ToastProvider>
           <AuthProvider>
